2b7b5e7d6af1c80f6ba678832f71b8aaa84506a3
Loïc CAILLE <loccaille@gmail.com>
2024-10-16 13:25:22 +0200

    init

--- src/components/AccountTypeItem.tsx
+++ src/components/AccountTypeItem.tsx
@@ -1,17 +1,19 @@
 type AccountTypeItemProps = {
     title: string;
     description: string;
+    onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
-const AccountTypeItem = ({title, description} : AccountTypeItemProps) => {
+const AccountTypeItem = ({title, description, onClick} : AccountTypeItemProps) => {
+    
     return ( 
-        <div style={{cursor:'pointer'}} className='flex flex-row align-middle gap-x-4 p-4'>
+        <div onClick={onClick} style={{cursor:'pointer'}} className='flex flex-row align-middle gap-x-4 p-4'>
               <img src="./src/assets/account_type.svg" alt="" />
               <div className='flex flex-col text-start justify-center'>
                 <h4 className='font-bold'>{title}</h4>
                 <p className='text-gray-300'>{description}</p>
               </div>
-            </div>
+        </div>
      );
 }
  
